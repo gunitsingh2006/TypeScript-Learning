3850d2618b45ecc741803aa5726b919373f6fe2b
Gunit Singh <gunitsingh.duggal@gmail.com>
2025-01-28 19:17:31 +0530

    use case

---
+++ types.js
@@ -0,0 +1,9 @@
+// USE CASE
+var myUser = {
+    _id: "6969",
+    name: "gunit",
+    email: "gunit.com",
+    isActive: false
+};
+myUser.email = "gunitsingh.com";
+console.log(myUser);
--- types.ts
+++ types.ts
@@ -12,6 +12,19 @@ let myUser: User ={
     isActive :false
 }
 
+type cardNumber={
+    cardnumber:number
+}
+
+type cardDates={
+    carddates:string
+}
+
+type cardDetails=cardNumber&cardDates&{        // this is use case where you define a new type based on the exsistance type
+    cvv:number
+}
+
+
 myUser.email = "gunitsingh.com"
 console.log(myUser)
 
