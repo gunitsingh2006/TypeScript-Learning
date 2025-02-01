eb3b8f6815c229edda387dbeb2f1997d09e871ba
Gunit Singh <gunitsingh.duggal@gmail.com>
2025-02-01 23:24:14 +0530

    enum

--- myEnum.ts
+++ myEnum.ts
@@ -12,9 +12,9 @@
 // Enum definition
 // The enum keyword is used to define an enumeration, a distinct type that consists of a set of named constants called the members of the enum.
 enum SeatChoice{
-    AISLE,
-    MIDDLE,
-    WINDOW
+    AISLE,  // 0             if we do  = 10
+    MIDDLE, // 1             11
+    WINDOW  //2              12
 }
 
 const hcSeat = SeatChoice.AISLE
